2f1afdac37b6305fdf625d39edf641472ae45fac
Dormoxx <dormoxx@gmail.com>
2023-06-25 19:47:07 -0400

    formatted curve layout, added component count to wealth calc
    Made SimpleCurve declarations easier to read, added comments referencing vanilla stats and starting wealth, components added to wealth calculation, wrote description.

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -8,12 +8,44 @@ namespace TinaTechnology
     [HarmonyPatch(typeof(Map), "get_PlayerWealthForStoryteller")]
     public static class TechIsWealth
     {
-        static SimpleCurve wealthCurve = new SimpleCurve(new CurvePoint[] { new CurvePoint(0, 0), new CurvePoint(3800, 0), new CurvePoint(150000, 400000f), new CurvePoint(420000, 700000f), new CurvePoint(666666, 1000000f) });
-        static SimpleCurve techBuildingCurve = new SimpleCurve(new CurvePoint[] { new CurvePoint(0, 0), new CurvePoint(10, 5000), new CurvePoint(100, 25000), new CurvePoint(1000, 150000) });
+        /*
+         * vanilla starter wealth:
+         *      Tribal: 2800 research, 0 threat
+         *      Crashlanded: 4100 research, ~8982 threat points, 30 components
+         * modpack starter wealth:
+         *      Tribal: 3200 research
+         *      Crashlanded: 3582 research, ~7533 threat points, 30 components
+         * 
+         * vanilla points per wealth curve:
+         *      (0, 0)
+         *      (14000, 0)
+         *      (400000, 2,400)
+         *      (700000, 3,600)
+         *      (1000000, 4,200)
+         */
+        static SimpleCurve wealthCurve = new SimpleCurve(
+            new CurvePoint[] { 
+                new CurvePoint(0, 0),
+                new CurvePoint(5000, 0),
+                new CurvePoint(150000, 400000f),
+                new CurvePoint(420000, 700000f),
+                new CurvePoint(666666, 1000000f)
+            });
+        static SimpleCurve techBuildingCurve = new SimpleCurve(
+            new CurvePoint[] {
+                new CurvePoint(0, 0),
+                new CurvePoint(10, 5000),
+                new CurvePoint(100, 25000),
+                new CurvePoint(1000, 150000)
+            });
         const int AdvancedComponentMultiplier = 10;
 
         public static void Postfix(Map __instance, ref float __result)
         {
+            #if DEBUG
+                Log.Message("Wealth calculated as " + __result + " before Tina Technology Postfix");
+            #endif
+
             if (Find.Storyteller.def != DataBank.StorytellerDefOf.Tina)
                 return;
 
@@ -28,20 +60,22 @@ namespace TinaTechnology
                     numTechBuildings += AdvancedComponentMultiplier;
             }
 
-            /*
-            int numComponents = 0;
+
+            float numComponents = 0;
             foreach (Thing t in __instance.listerThings.ThingsOfDef(ThingDefOf.ComponentIndustrial))
-                numComponents += t.stackCount;
+                numComponents += (t.stackCount * 0.5f);
             foreach (Thing t in __instance.listerThings.ThingsOfDef(ThingDefOf.ComponentSpacer))
-                numComponents += t.stackCount * AdvancedComponentMultiplier;
-            Log.Message("numComponents == " + numComponents);
-            */
+                numComponents += ((t.stackCount * AdvancedComponentMultiplier) * 0.5f);
+
 
             wealth += techBuildingCurve.Evaluate(numTechBuildings);
+            wealth += techBuildingCurve.Evaluate(numComponents);
             __result = wealthCurve.Evaluate(wealth);
             #if DEBUG
-            Log.Message("Tina Technology calculates threat points should be " + __result + " based on " + 
-                ResearchToWealth() + " research and " + numTechBuildings + " component-based buildings");
+            Log.Message("Tina Technology calculates threat points should be " + __result + 
+                        " based on " + ResearchToWealth() + " research, "
+                        + numTechBuildings + " component-based buildings and "
+                        + numComponents * 2 + " components.");
             #endif
         }
 
@@ -50,11 +84,13 @@ namespace TinaTechnology
             float num = 0;
             foreach (ResearchProjectDef proj in DefDatabase<ResearchProjectDef>.AllDefs)
             {
-                //using proj.CostApparent instead of proj.baseCost for compat with tech advancing
-                //as that mod changes research cost based on tech level
+                //using proj.CostApparent instead of proj.baseCost for compat with
+                //tech advancing as that mod changes research cost based on tech level
                 if (proj.IsFinished)
                     num += proj.CostApparent; 
             }
+            //vanilla total research tree == 190,000
+            //modpack total reseach tree == 292,139.1 
             if (num > 100000)
             {
                 #if DEBUG
